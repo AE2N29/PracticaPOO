074b976183ac3bd9c175d7be3c1433a107ad2e4a
Jose <josehdezzz06@gmail.com>
2025-09-17 20:12:30 +0200

    prueba commit

---
+++ src/main/java/es/upm/etsisi/poo/App.java
@@ -0,0 +1,12 @@
+package es.upm.etsisi.poo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "¡Hola!" );
+    }
+}
