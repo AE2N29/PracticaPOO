fd8c8146d24cca38ab5881f24c99cd064edd3c78
Jose <josehdezzz06@gmail.com>
2025-09-22 19:54:32 +0200

    Añadida la clase Product con los metodos get, set y toString, el enum Category y la clase Catalog para almacenar los productos que se quieran añadir. No todo esta completo

---
+++ src/main/java/es/upm/etsisi/poo/Product.java
@@ -0,0 +1,52 @@
+package es.upm.etsisi.poo;
+
+public class Product {
+    private int identifier;
+    private String name;
+    private Category category;
+    private double price;
+
+    public Product(int identifier, String name, Category category, double price) {
+        this.identifier = identifier;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public int getIdentifier() {
+        return identifier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setIdentifier(int identifier) {
+        this.identifier = identifier;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Product: " + this.name + " | Category: " + this.category.name()
+                + " | Price: " + this.price + " | Identifier: " + identifier;
+    }
+}
