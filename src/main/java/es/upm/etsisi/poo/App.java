47860c7bd04224bfd352163df45fe450139e8af1
josehgar <josehdezzz06@gmail.com>
2025-09-26 11:58:57 +0200

    borre sin querer el metodo help. Lo he vuelto a poner

--- src/main/java/es/upm/etsisi/poo/App.java
+++ src/main/java/es/upm/etsisi/poo/App.java
@@ -27,6 +27,25 @@ public class App {
         System.out.println("Goodbye!");
     }
 
+    public void help() {
+        System.out.println("Commands:");
+        System.out.println(" prod add <id> \"<name>\" <category> <price>");
+        System.out.println(" prod list");
+        System.out.println(" prod update <id> NAME|CATEGORY|PRICE <value>");
+        System.out.println(" prod remove <id>");
+        System.out.println(" ticket new");
+        System.out.println(" ticket add <prodId> <quantity>");
+        System.out.println(" ticket remove <prodId>");
+        System.out.println(" ticket print");
+        System.out.println(" echo \"<texto>\"");
+        System.out.println(" help");
+        System.out.println(" exit");
+        System.out.println("");
+        System.out.println("Categories: MERCH, STATIONERY, CLOTHES, BOOK, ELECTRONICS");
+        System.out.println("Discounts if there are ≥2 units in the category: MERCH 0%, STATIONERY 5%, CLOTHES 7%, BOOK 10%,");
+        System.out.println("ELECTRONICS 3%.");
+    }
+
     private String typeCommand() {  // To make easier the implementation of the shell
         Scanner sc = new Scanner(System.in);
         System.out.print("tUpm> ");
