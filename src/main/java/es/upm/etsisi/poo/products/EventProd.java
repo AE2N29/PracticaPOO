589297b69994a78067334df718ed9402b67e5ddc
Brandon Heinricks <brandon.heinricks@gmail.com>
2025-11-25 11:09:28 +0100

    Crecaion del paquete con clases proucto para la nueva implementacion. Hace falta revision

---
+++ src/main/java/es/upm/etsisi/poo/products/EventProd.java
@@ -0,0 +1,42 @@
+package es.upm.etsisi.poo.products;
+
+import java.time.LocalDateTime;
+
+public abstract class EventProd extends AbstractProduct {
+    protected static final int MAX_PEOPLE = 100;
+    protected int personNumber;
+    protected LocalDateTime expirationDate;
+    protected double pricePerPerson;
+
+    public EventProd(String id, String name, LocalDateTime expirationDate, double pricePerPerson,  int MAX_PEOPLE, int personNumber) {
+        super(id, name);
+
+        if(personNumber > MAX_PEOPLE) {
+            throw new IllegalArgumentException("personNumber > 100");
+        }
+
+        this.expirationDate = expirationDate;
+        this.pricePerPerson = pricePerPerson;
+        this.personNumber = personNumber;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+    public double getPricePerPerson() {
+        return pricePerPerson;
+    }
+    public int getPersonNumber() {
+        return personNumber;
+    }
+
+    @Override
+    protected boolean availability() {
+        return LocalDateTime.now().isBefore(expirationDate);
+    }
+
+    @Override
+    protected double calculatePrice() {
+        return pricePerPerson*personNumber;
+    }
+}
---
+++ src/main/java/es/upm/etsisi/poo/products/StockProducts.java
@@ -0,0 +1,38 @@
+package es.upm.etsisi.poo.products;
+
+import es.upm.etsisi.poo.Category;
+
+public class StockProducts extends AbstractProduct {
+    protected Category category;
+    protected double price;
+
+
+    public StockProducts(String name, String id, Category category, double price) {
+        super(id, name);
+        this.category = category;
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+    public double getPrice() {
+        return calculatePrice();
+    }
+
+    @Override
+    protected double calculatePrice() { // el precio es el mismo siempre, a diferencia con los personalizables
+        return price;
+    }
+
+    @Override
+    protected boolean availability() { //siempre true, no ha que validar nada como en los eventos
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "{class:Product, id:" + this.id + ", name: '" + this.name + "', category:" + this.category.name() +
+                ", price:" + String.format("%.1f", this.price) + "}";  // se usa string.format para devolver solo un decimal
+    }
+}
