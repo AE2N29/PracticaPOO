58c703c626547857334ed5945a9fda106653c8f3
josehgar <josehdezzz06@gmail.com>
2026-01-16 15:35:15 +0100

    mensajes de error de prodadd y prodmeeting arreglados

--- src/main/java/es/upm/etsisi/poo/utils/InputValidator.java
+++ src/main/java/es/upm/etsisi/poo/utils/InputValidator.java
@@ -45,9 +45,7 @@ public class InputValidator {
         switch (splittedCommand[1]) {
             case "ADD":
                 return validateProdAdd(fullCommand);
-            case "ADDFOOD":
-                return validateProdAddEvent(fullCommand);
-            case "ADDMEETING":
+            case "ADDFOOD", "ADDMEETING":
                 return validateProdAddEvent(fullCommand);
             case "UPDATE":
                 return validateProdUpdate(fullCommand);
@@ -144,8 +142,7 @@ public class InputValidator {
                 double price = Double.parseDouble(processedCommand[priceIndex]);
                 if(price < 0) {return false;}
                 int maxPeople = Integer.parseInt(processedCommand[maxPeopleIndex]);
-                if(maxPeople <= 0  || maxPeople> 100){return false;}
-                return true;
+                return maxPeople > 0;
             }
             return false;
         }catch (Exception e) {
